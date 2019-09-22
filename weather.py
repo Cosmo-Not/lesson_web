29b83a6f2faeddf0354129801a1d51cf8f83d2f9
Yuri Astafev <tyler333@mail.ru>
2019-09-22 23:57:26 +0300

    change

--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@ import requests
 def weather_by_city(city_name):
     weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
     params = {
+        "key": "97055bf6ef144b11a59201042192209",
         "q": city_name,
         "format": "json",
         "num_of_days": 1,
