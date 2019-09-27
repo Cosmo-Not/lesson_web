34ab5b881bae6819cf4c120b1923ee0e72b0f607
Yuri Astafev <tyler333@mail.ru>
2019-09-27 08:00:47 +0300

    template

--- server.py
+++ server.py
@@ -1,30 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = 'Новости Python'
     weather = weather_by_city("Moscow,Russia")
-    if weather:
-        weather_text = f'Погода: {weather["temp_C"]}, ощущается как {weather["FeelsLikeC"]}'
-    else:
-        weather_text = "Сервис погоды временно недоступен"
-    return f"""
-    <html>
-        <head>
-            <title>Прогноз погоды</title>
-        </head>
-        <body>
-            <h1>{weather_text}</h1>
-            <ol>
-                <li>Один</li>
-                <li>Два</li>
-                <li>Три</li>
-            </ol>
-        </body>
-    </html>
-    """
+    return render_template('index.html', page_title = title, weather = weather)
 
 if __name__ == "__main__":
     app.run(debug = True)
